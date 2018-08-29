58b40944ac9fc7332094239365554c6ef2986ab8
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-08-29 19:06:15 -0300

    Update pre-install.bash

--- pre-install.bash
+++ pre-install.bash
@@ -4,6 +4,10 @@
 dnf update -y -x kernel-*
 dnf install -y docker git vim bash-completion curl
 
+# docker vagrant user
+groupadd docker
+usermod -aG docker vagrant
+
 # docker service
 systemctl enable docker
 systemctl start docker
