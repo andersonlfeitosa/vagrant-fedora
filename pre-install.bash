a33c486c583b4e2cd2743921efdf34c7c4c065b1
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-09-04 08:52:32 -0300

    make it better

--- pre-install.bash
+++ pre-install.bash
@@ -8,12 +8,12 @@ dnf install -y docker git vim curl dos2unix zip unzip bash-completion
 groupadd docker
 usermod -aG docker vagrant
 
+# copy files
+cp etc/docker/daemon.json /etc/docker/daemon.json
+
 # docker service
 systemctl enable docker
 systemctl start docker
-
-# copy files
-cp etc/docker/daemon.json /etc/docker/daemon.json
 
 # install openshift
 wget https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz
