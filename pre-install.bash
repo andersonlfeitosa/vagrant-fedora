6a4c92b07560271cf1d9d93c181ab66f4012a58f
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-09-04 09:15:05 -0300

    make it better

--- pre-install.bash
+++ pre-install.bash
@@ -12,8 +12,3 @@ usermod -aG docker vagrant
 systemctl enable docker
 systemctl start docker
 
-# install openshift
-wget https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz
-tar -xvzf openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz
-mv openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit/oc /usr/bin
-rm -rf openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit 
